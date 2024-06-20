273817e7bded85c1a056b2293bb0655a507f9ea5
unknown <jl1429@rit.edu>
2024-06-20 13:08:53 -0400

    learning

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+
+function Square({value, onSquareClick}){
+
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
+
+export default Square;
