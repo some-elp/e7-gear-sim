025d8282d8bea9dc203fb3e2b30d52ddd21086fc
unknown <jl1429@rit.edu>
2024-06-28 09:38:31 -0400

    I am an idiot

--- src/SubstatDropdown.js
+++ src/SubstatDropdown.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 export default function SubstatDropdown(){
-    const [substat, setSubstat] = useState('Select Substat');
+    const [substat, setSubstat] = useState("Select Substat");
     return(
         <form>
             <label>Substats:</label>
