2f3f7b0ae480dc662696521e221b2da179a3489e
unknown <ineedelp66@gmail.com>
2024-06-29 10:14:39 -0400

    disappearing substat dropdowns test

--- src/Gacha.js
+++ src/Gacha.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import SubstatDropdown from "./SubstatDropdown"
 
 export default function Gacha(){
+    const [iLevel, setILevel] = useState('');
 
     return (
         <div>
+            <label>Equipment Level</label>
+            <select
+                value={iLevel}
+                onChange={(e) => setILevel(e.target.value)}
+            >
+                <option value="85">85</option>
+                <option value="88">88</option>
+            </select>
+            <p>{iLevel}</p>
             <label>Substats:</label>
-            <SubstatDropdown/>
-            <SubstatDropdown/>
-            <SubstatDropdown/>
             <SubstatDropdown/>
         </div>
     )
--- src/SubstatDropdown.js
+++ src/SubstatDropdown.js
@@ -1,27 +1,100 @@
 import { useState } from "react";
 
-export default function SubstatDropdown(){
-    const [substat, setSubstat] = useState("");
-    return(
-        <form>
-            <select 
-                value={substat}
-                onChange={(e) => setSubstat(e.target.value)}
-            >
-                <option value="attack%">Attack%</option>
-                <option value="defense%">Defense%</option>
-                <option value="hp%">Health%</option>
-                <option value="speed">Speed</option>
-                <option value="critchance">Crit Chance</option>
-                <option value="critdamage">Crit Damage</option>
-                <option value="effectiveness">Effectiveness</option>
-                <option value="effectresist">Effect Resistance</option>
-                <option value="attack">Flat Attack</option>
-                <option value="defense">Flat Defense</option>
-                <option value="hp">Flat Health</option>
-            </select>
-            <p>{substat}</p>
-        </form>
+export default function SubstatDropdown({ }) {
+    const [substats, setSubstats] = useState({
+        substat1: "",
+        substat2: "",
+        substat3: "",
+        substat4: ""
+    });
+
+    function handleSelect(event) {
+        const { name, value } = event.target;
+        setSubstats({
+            ...substats,
+            [name]: value,
+        });
+    };
+
+    const allSelected = Object.values(substats).every(selection => selection !== '');
+
+    return (
+        <div>
+            {!allSelected && (
+            <form>
+                <select
+                    name="substat1"
+                    value={substats.substat1}
+                    onChange={handleSelect}
+                >
+                    <option value="attack%">Attack%</option>
+                    <option value="defense%">Defense%</option>
+                    <option value="hp%">Health%</option>
+                    <option value="speed">Speed</option>
+                    <option value="critchance">Crit Chance</option>
+                    <option value="critdamage">Crit Damage</option>
+                    <option value="effectiveness">Effectiveness</option>
+                    <option value="effectresist">Effect Resistance</option>
+                    <option value="attack">Flat Attack</option>
+                    <option value="defense">Flat Defense</option>
+                    <option value="hp">Flat Health</option>
+                </select>
+                <select
+                    name="substat2"
+                    value={substats.substat2}
+                    onChange={handleSelect}
+                >
+                    <option value="attack%">Attack%</option>
+                    <option value="defense%">Defense%</option>
+                    <option value="hp%">Health%</option>
+                    <option value="speed">Speed</option>
+                    <option value="critchance">Crit Chance</option>
+                    <option value="critdamage">Crit Damage</option>
+                    <option value="effectiveness">Effectiveness</option>
+                    <option value="effectresist">Effect Resistance</option>
+                    <option value="attack">Flat Attack</option>
+                    <option value="defense">Flat Defense</option>
+                    <option value="hp">Flat Health</option>
+                </select>
+                <select
+                    name="substat3"
+                    value={substats.substat3}
+                    onChange={handleSelect}
+                >
+                    <option value="attack%">Attack%</option>
+                    <option value="defense%">Defense%</option>
+                    <option value="hp%">Health%</option>
+                    <option value="speed">Speed</option>
+                    <option value="critchance">Crit Chance</option>
+                    <option value="critdamage">Crit Damage</option>
+                    <option value="effectiveness">Effectiveness</option>
+                    <option value="effectresist">Effect Resistance</option>
+                    <option value="attack">Flat Attack</option>
+                    <option value="defense">Flat Defense</option>
+                    <option value="hp">Flat Health</option>
+                </select>
+                <select
+                    name="substat4"
+                    value={substats.substat4}
+                    onChange={handleSelect}
+                >
+                    <option value="attack%">Attack%</option>
+                    <option value="defense%">Defense%</option>
+                    <option value="hp%">Health%</option>
+                    <option value="speed">Speed</option>
+                    <option value="critchance">Crit Chance</option>
+                    <option value="critdamage">Crit Damage</option>
+                    <option value="effectiveness">Effectiveness</option>
+                    <option value="effectresist">Effect Resistance</option>
+                    <option value="attack">Flat Attack</option>
+                    <option value="defense">Flat Defense</option>
+                    <option value="hp">Flat Health</option>
+                </select>
+                <p>{substats}</p>
+            </form>
+            )}
+            {allSelected && (<p>{substats}</p>)}
+        </div>
     );
 }
 
