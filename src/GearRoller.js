f0ea1f86ffe7bbaeb2da556a9e59bd43a46c4c2a
unknown <ineedelp66@gmail.com>
2024-07-02 16:55:33 -0400

    limit max number of enhances to 5

--- src/GearRoller.js
+++ src/GearRoller.js
@@ -1,8 +1,10 @@
-function GearRoller({enhancement, substats, textInputs, gearLevel, gearTier, handleEnhancement}) {
-    //TODO: iLevel difference, Purple/Red gear difference.
+import { useState } from "react";
+
+function GearRoller({ enhancement, substats, textInputs, gearLevel, gearTier, handleEnhancement }) {
+    //TODO: iLevel difference seems to work. Purple gear next.
 
     console.log('GearRoller props:', { substats, gearLevel, gearTier, textInputs, handleEnhancement });
-    let enhanceLevel = 0;
+    const [enhanceCount, setEnhanceCount] = useState(0);
 
 
     /*
@@ -16,7 +18,7 @@ function GearRoller({enhancement, substats, textInputs, gearLevel, gearTier, han
         let substatKeys = Object.keys(substats);
         let randomSubstat = substatKeys[Math.floor(Math.random() * substatKeys.length)];
         let substatName = substats[randomSubstat];
-        let inputHistory = {...textInputs};
+        let inputHistory = { ...textInputs };
 
         if (substatName && enhancement[substatName] && enhancement[substatName][gearLevel]) {
             const { values, weights } = enhancement[substatName][gearLevel][gearTier];
@@ -24,29 +26,25 @@ function GearRoller({enhancement, substats, textInputs, gearLevel, gearTier, han
 
             inputHistory[randomSubstat] = (parseInt(inputHistory[randomSubstat])) + increment;
             handleEnhancement(inputHistory);
+            setEnhanceCount(enhanceCount + 1);
         }
         console.log(substats);
         console.log(textInputs);
-        enhanceLevel = enhanceLevel + 3;
         console.log(enhanceLevel);
 
     }
 
-    if (enhanceLevel < 15) {
+    return (
+        <>
+            <p>{substats.substat1}: {textInputs.substat1}</p>
+            <p>{substats.substat2}: {textInputs.substat2}</p>
+            <p>{substats.substat3}: {textInputs.substat3}</p>
+            <p>{substats.substat4}: {textInputs.substat4}</p>
+            <button onClick={enhanceGear} disabled={enhanceCount >= 5}>Enhance</button>
+            {enhanceCount >= 5 && <p>Satisfied?</p>}
+        </>
+    );
 
-        return (
-            <>
-                <p>{substats.substat1}: {textInputs.substat1}</p>
-                <p>{substats.substat2}: {textInputs.substat2}</p>
-                <p>{substats.substat3}: {textInputs.substat3}</p>
-                <p>{substats.substat4}: {textInputs.substat4}</p>
-                <button onClick={enhanceGear}>Enhance</button>
-            </>
-        );
-    }
-    else {
-        return <p>Satisfied?</p>
-    }
 }
 
 function randomIncrease(values, weights) {
