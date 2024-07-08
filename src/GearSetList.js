8c384ac0c2f3966204b1f9db173addf08d4c8fb2
unknown <ineedelp66@gmail.com>
2024-07-08 11:42:38 -0400

    redeploy

--- src/GearSetList.js
+++ src/GearSetList.js
@@ -1,11 +1,7 @@
 const GearSetList = {
     "attack": ["PvE DPS", "Burn/Pure Attack-Scaling DPS", "Attack-Scaling Bruiser", "DPS"],
-    "defense": {
-
-    },
-    "health": {
-
-    },
+    "defense": ["Soulweaver/Tank", "Defense-Scaling Bruiser",],
+    "health": ["Soulweaver/Tank", "HP-Scaling Bruiser", "Attack-Scaling Bruiser"],
     "speed": {
 
     },
