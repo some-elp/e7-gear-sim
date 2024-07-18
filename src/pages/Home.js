0b77fd9aad332323d799953d7ddb73ac614e7895
unknown <ineedelp66@gmail.com>
2024-07-18 10:53:28 -0400

    trying to make bottom part of artena image fade to black

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 import "../css/home.css"
 
 export default function Home() {
-
     //Using this or else all pages default to one body
     useBodyClass('home-body');
 
     return (  
         <div className="home-container">
-            <img src="./images/artena_idle.png" className="artena"/>
+            <img src="./images/artena_idle.png" alt="" className="artena"/>
             <div className="buttons-container">
                 <Link to="/gacha" style={{textDecoration: 'none'}}><button>
                     Gear Roll Gacha
